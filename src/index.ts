cb809104e9fd288262f306717ef4bd18cca9a21e
Marco Eckstein <marco.eckstein@gmx.de>
2018-08-09 03:44:19 +0200

    Forgotten in commit 7f5de58cada2d55420515effc10d82255348bb51

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { DateUtil } from "./DateUtil";
 export { DomUtil } from "./DomUtil";
 export { GeoUtil, LatitudeLongitude } from "./GeoUtil";
 export { I18nUtil } from "./I18nUtil";
+export { NavigatorUtil } from "./NavigatorUtil";
 export { DayOfWeek, OpeningTimesUtil, TimeInterval } from "./OpeningTimesUtil";
